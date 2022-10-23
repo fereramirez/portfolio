3bb923f0ab9d8cf99813fbe4a7f6ee72d236937a
Fer Ramirez <fer.eze.ram@gmail.com>
2022-10-23 18:26:36 -0300

    footer +, mobile home button +

---
+++ src/components/Projects/Footer.jsx
@@ -0,0 +1,23 @@
+import { ReactComponent as Github } from "../../assets/svg/github.svg";
+
+import css from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={css.container}>
+      <a
+        href={"https://github.com/fereramirez/portfolio"}
+        target="_blank"
+        rel="noreferrer"
+        className={css.link}
+      >
+        Portfolio Repo
+        <div className={css.svgContainer}>
+          <Github />
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
