f816d6ce0ef957673613529ae94b9e60ae17ab2a
Fer Ramirez <fer.eze.ram@gmail.com>
2022-10-25 00:54:46 -0300

    intro +

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className={css.App}>
       <LanguageProvider>
-        {/* <Intro /> */}
+        <Intro />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
