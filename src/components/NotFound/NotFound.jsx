8e470d52623034c7182d156d5c277e69131d0568
Fernando <fer.eze.ram@gmail.com>
2024-01-30 19:02:29 -0300

    Update NotFound css class

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -3,22 +3,22 @@ import css from "./NotFound.module.css";
 function NotFound() {
   return (
     <div className={css.notFoundContainer}>
-      <div className={css.text} title="ERROR 404: Not Found">
+      <div className={css.notFoundText} title="ERROR 404: Not Found">
         ERROR 404:
       </div>
-      <div className={css.text} title="ERROR 404: Not Found">
+      <div className={css.notFoundText} title="ERROR 404: Not Found">
         Not Found
       </div>
-      <div className={css.text} title="ERROR 404:">
+      <div className={css.notFoundText} title="ERROR 404:">
         ERROR 404:
       </div>
-      <div className={css.text} title="Not Found">
+      <div className={css.notFoundText} title="Not Found">
         Not Found
       </div>
-      <div className={css.text} title="Not Found">
+      <div className={css.notFoundText} title="Not Found">
         ERROR 404:
       </div>
-      <div className={css.text} title="ERROR 404:">
+      <div className={css.notFoundText} title="ERROR 404:">
         Not Found
       </div>
     </div>
