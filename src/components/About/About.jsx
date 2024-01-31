69d56804898798472871792532bd046e0565a493
Fernando <fer.eze.ram@gmail.com>
2024-01-31 15:11:02 -0300

    Migrate About to scss

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,35 +6,34 @@ import AboutTechnologies from "./AboutTechnologies";
 import { ReactComponent as Cv } from "../../assets/svg/cv.svg";
 
 import "../../App.css";
-import css from "./About.module.css";
+import "./About.scss";
 
 const About = () => {
   const { texts } = useContext(LanguageContext);
 
   return (
-    <div className={`${css.container} content`}>
-      <h1 className={css.title}>{texts.about.title}</h1>
+    <main className="about-container content">
+      <h1 className="title">{texts.about.title}</h1>
 
-      <div className={css.paragraph}>
+      <div className="paragraph">
         <span>
-          {texts.about.im}{" "}
-          <strong className={css.name}>Fernando Ramirez</strong>
+          {texts.about.im} <strong className="name">Fernando Ramirez</strong>
           {texts.about.firstParagraph}
         </span>
       </div>
 
-      <div className={css.paragraph}>{texts.about.secondParagraph}</div>
-      <div className={css.paragraph}>{texts.about.thirdParagraph}</div>
-      <div className={css.paragraph}>{texts.about.technologies}</div>
+      <div className="paragraph">{texts.about.secondParagraph}</div>
+      <div className="paragraph">{texts.about.thirdParagraph}</div>
+      <div className="paragraph">{texts.about.technologies}</div>
 
       <AboutTechnologies />
 
-      <div className={css.paragraph}>
+      <div className="paragraph">
         <a
           href="https://drive.google.com/file/d/1Jc8UyhxUJGqPA4FFnPPN86OUCnVJ3ifc/view?usp=share_link"
           target="_blank"
           rel="noreferrer"
-          className={css.resumeLink}
+          className="resume-link"
         >
           <span>{texts.about.resume}</span>
 
@@ -43,7 +42,7 @@ const About = () => {
           </span>
         </a>
       </div>
-    </div>
+    </main>
   );
 };
 
