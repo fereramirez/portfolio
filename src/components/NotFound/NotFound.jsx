1f3559c7458f17b0d4e2ea733b0fc73cb4db6324
Fernando <fer.eze.ram@gmail.com>
2024-01-30 22:28:26 -0300

    Migrate NotFound to scss

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,24 +1,25 @@
-import css from "./NotFound.module.css";
+import "./NotFound.scss";
 
 const NotFound = () => {
+  //! VOLVER A VER al entrar a un enlace 404, por un instante se ve el contenido de NotFound
   return (
-    <div className={css.notFoundContainer}>
-      <div className={css.notFoundText} title="ERROR 404: Not Found">
+    <div className="not-found-container">
+      <div className="not-found-text" title="ERROR 404: Not Found">
         ERROR 404:
       </div>
-      <div className={css.notFoundText} title="ERROR 404: Not Found">
+      <div className="not-found-text" title="ERROR 404: Not Found">
         Not Found
       </div>
-      <div className={css.notFoundText} title="ERROR 404:">
+      <div className="not-found-text" title="ERROR 404:">
         ERROR 404:
       </div>
-      <div className={css.notFoundText} title="Not Found">
+      <div className="not-found-text" title="Not Found">
         Not Found
       </div>
-      <div className={css.notFoundText} title="Not Found">
+      <div className="not-found-text" title="Not Found">
         ERROR 404:
       </div>
-      <div className={css.notFoundText} title="ERROR 404:">
+      <div className="not-found-text" title="ERROR 404:">
         Not Found
       </div>
     </div>
