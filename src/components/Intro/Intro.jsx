ee6080f88b1f55f5763ddb0782980601eb87444b
Fernando <fer.eze.ram@gmail.com>
2024-02-02 19:41:51 -0300

    Add useIntro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,46 +1,16 @@
-import { useEffect, useState, useContext } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { useContext } from "react";
+import { Outlet } from "react-router-dom";
 
+import useIntro from "../../hooks/useIntro";
 import AnimationContext from "../../context/AnimationContext";
 import Grid from "../common/Grid";
 
 import "./Intro.scss";
 
 const Intro = () => {
-  const [topText, setTopText] = useState("");
-  const [bottomText, setBottomText] = useState("");
-  const [bottomCursor, setBottomCursor] = useState(false);
-  const location = useLocation();
   const { runFadeOut, runPop } = useContext(AnimationContext);
 
-  useEffect(() => {
-    document.documentElement.style.overflow = "hidden";
-    const name = "Fernando Ramirez,";
-    const title = "Fullstack Developer.";
-
-    let i = 0;
-    let j = 0;
-
-    setInterval(() => {
-      setTopText(name.slice(0, i + 1));
-      i += 1;
-    }, 60);
-
-    setTimeout(() => {
-      setInterval(() => {
-        setBottomText(title.slice(0, j + 1));
-        j += 1;
-      }, 60);
-    }, 2000);
-
-    setTimeout(() => {
-      setBottomCursor(true);
-    }, 2000);
-
-    setTimeout(() => {
-      document.documentElement.style.overflowY = "auto";
-    }, 8000);
-  }, []);
+  const { topText, bottomText, bottomCursor, location } = useIntro();
 
   return (
     <>
---
+++ src/hooks/useIntro.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const useIntro = () => {
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
+  const [bottomCursor, setBottomCursor] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    document.documentElement.style.overflow = "hidden";
+    const name = "Fernando Ramirez,";
+    const title = "Fullstack Developer.";
+
+    let i = 0;
+    let j = 0;
+
+    setInterval(() => {
+      setTopText(name.slice(0, i + 1));
+      i += 1;
+    }, 60);
+
+    setTimeout(() => {
+      setInterval(() => {
+        setBottomText(title.slice(0, j + 1));
+        j += 1;
+      }, 60);
+    }, 2000);
+
+    setTimeout(() => {
+      setBottomCursor(true);
+    }, 2000);
+
+    setTimeout(() => {
+      document.documentElement.style.overflowY = "auto";
+    }, 8000);
+  }, []);
+
+  return { topText, bottomText, bottomCursor, location };
+};
+
+export default useIntro;
