3260ea3c7af7dac88906bb94f7bb74af1a2b3307
Fernando <fer.eze.ram@gmail.com>
2024-09-02 16:21:40 -0300

    Update current technologies

--- src/components/About/AboutTechnologies.jsx
+++ src/components/About/AboutTechnologies.jsx
@@ -1,19 +1,19 @@
 /* import { ReactComponent as HTML } from "../../assets/svg/html.svg";
-import { ReactComponent as CSS } from "../../assets/svg/css.svg";
-import { ReactComponent as JS } from "../../assets/svg/js.svg"; */
+import { ReactComponent as CSS } from "../../assets/svg/css.svg"; */
+import { ReactComponent as JS } from "../../assets/svg/js.svg";
 import { ReactComponent as ReactSvg } from "../../assets/svg/react.svg";
-import { ReactComponent as Redux } from "../../assets/svg/redux.svg";
+/* import { ReactComponent as Redux } from "../../assets/svg/redux.svg"; */
 import { ReactComponent as Nodejs } from "../../assets/svg/nodejs.svg";
 import { ReactComponent as Express } from "../../assets/svg/express.svg";
-import { ReactComponent as PostgresSql } from "../../assets/svg/postgresql.svg";
+/* import { ReactComponent as PostgresSql } from "../../assets/svg/postgresql.svg"; */
 import { ReactComponent as Mongo } from "../../assets/svg/mongo.svg";
-import { ReactComponent as Sequelize } from "../../assets/svg/sequelize.svg";
+/* import { ReactComponent as Sequelize } from "../../assets/svg/sequelize.svg"; */
 /* import { ReactComponent as Git } from "../../assets/svg/git.svg";
 import { ReactComponent as Github } from "../../assets/svg/github.svg"; */
-import { ReactComponent as Chakra } from "../../assets/svg/chakraui.svg";
+/* import { ReactComponent as Chakra } from "../../assets/svg/chakraui.svg"; */
 import { ReactComponent as CssModules } from "../../assets/svg/cssmodules.svg";
 import { ReactComponent as Sass } from "../../assets/svg/sass.svg";
-// import { ReactComponent as Next } from "../../assets/svg/next.svg";
+import { ReactComponent as Next } from "../../assets/svg/next.svg";
 import { ReactComponent as Typescript } from "../../assets/svg/typescript.svg";
 
 import "./AboutTechnologies.scss";
@@ -29,11 +29,11 @@ const AboutTechnologies = () => {
           <div className="about-technology-container">
             <CSS />
             <div className="tooltip">CSS</div>
-          </div>
-          <div className="about-technology-container">
-            <JS />
-            <div className="tooltip">JavaScript</div>
           </div> */}
+        <div className="about-technology-container">
+          <JS />
+          <div className="tooltip">JavaScript</div>
+        </div>
 
         <div className="about-technology-container">
           <Typescript />
@@ -44,14 +44,14 @@ const AboutTechnologies = () => {
           <ReactSvg />
           <div className="tooltip">React.js</div>
         </div>
-        <div className="about-technology-container">
+        {/* <div className="about-technology-container">
           <Redux />
           <div className="tooltip">Redux.js</div>
-        </div>
-        <div className="about-technology-container">
+        </div> */}
+        {/* <div className="about-technology-container">
           <Chakra />
           <div className="tooltip">Chakra UI</div>
-        </div>
+        </div> */}
         <div className="about-technology-container">
           <span className="css-modules-svg-container">
             <CssModules />
@@ -84,19 +84,19 @@ const AboutTechnologies = () => {
           <Mongo />
           <div className="tooltip">MongoDB</div>
         </div>
-        <div className="about-technology-container">
+        {/* <div className="about-technology-container">
           <PostgresSql />
           <div className="tooltip">PostgreSQL</div>
         </div>
         <div className="about-technology-container">
           <Sequelize />
           <div className="tooltip">Sequelize</div>
-        </div>
+        </div> */}
 
-        {/* <div className="about-technology-container">
+        <div className="about-technology-container">
           <Next />
           <div className="tooltip">Next.js</div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
